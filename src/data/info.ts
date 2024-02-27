3f4e5a40b37a88f364495b9665764c644d1b4d23
Francisco Freire <contact.frann@gmail.com>
2024-02-27 12:41:16 -0300

    Initial Commit

---
+++ src/data/info.ts
@@ -0,0 +1,85 @@
+export const info = {
+	baseUrl: "https://frannfreire.com",
+	name: "Francisco Freire",
+	jobDescription: "Software Engineer",
+	about: `+4 years of experience. Software Engineer from Montevideo, Uruguay.
+  Specialized in cross-platform bots, web and mobile applications.`,
+
+	experience: [
+		{
+			name: "Hospocket (Remote)",
+			location: "United States",
+			startDate: "February 2021",
+			endDate: "April",
+			description: [
+				"✓ Developing screens and UI components for the web application using React and Chakra.",
+				"✓ Integrated all the responsive design and fixed bugs in the frontend.",
+			],
+		},
+
+		{
+			name: "VertexStudio (Remote)",
+			location: "El Salvador",
+			startDate: "Apr 2021",
+			endDate: "Present",
+			description: [
+				"✓ Web virtual reality for a metaverse platform in healthcare.",
+				"✓ Development of bots for discord communities.",
+				"✓ Others tasks connected to the nature of the job.",
+				"✓ Code Improvements and Refactoring.",
+			],
+		},
+	],
+
+	// education: [
+	//   {
+	//     name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
+	//     location: "Karāchi, Sindh, Pakistan",
+	//     startDate: "2019",
+	//     endDate: "2022",
+	//     description: [
+	//       "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+	//     ],
+	//   },
+	//   {
+	//     name: "Govt. Degree Boys College Buffer Zone",
+	//     location: "Karāchi, Sindh, Pakistan",
+	//     startDate: "2018",
+	//     endDate: "2020",
+	//     description: ["Intermediate, Pre-Engineering"],
+	//   },
+	//   {
+	//     name: "Ghazi Foundation Schools & Colleges",
+	//     location: "Karāchi, Sindh, Pakistan",
+	//     startDate: "2016",
+	//     endDate: "2018",
+	//     description: ["Matriculation,  Computer Science"],
+	//   },
+	// ],
+
+	socialMedia: {
+		twitter: "https://twitter.com/frannn_freire",
+		github: "https://github.com/frannfreire",
+		email: "mailto:contact.frann@gmail.com",
+		linkedin:
+			"https://www.linkedin.com/in/francisco-freire-anche%C3%B1o-472499178/",
+		instagram: "https://instagram.com/frann.freire",
+	},
+
+	projects: [
+		{
+			title: "My Portfolio",
+			isFeatured: true,
+			thumbnail: "/assets/images/astro-portfolio.png",
+			githubUrl: "",
+			liveUrl: "https://frannfreire.com/",
+		},
+		{
+			title: "Astro Portfolio",
+			isFeatured: true,
+			thumbnail: "/assets/images/astro-portfolio.png",
+			githubUrl: "",
+			liveUrl: "",
+		},
+	],
+};
